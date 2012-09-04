454dd5e891a7f8b264c24fe929e9dbfdf4217dc2
Cristian Rosu <redy_kdwa@yahoo.com>
2012-09-04 15:29:27 +0300

    setup+authorization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodCourt::Application.config.secret_token = 'eaeba00ce34cf617144db5e4a98e263e1829f7a8946339c4ef37c3144d0d1c1ddd8bff7c52f31b0e7fbba224ebf0f4af1d800ccd096e351fdda425a77ced77ca'
